9efeaf7768f04911d2665cdfa99bfd677ffa26d8
Sow Ying <66082627+chowsowying@users.noreply.github.com>
2024-03-05 14:49:09 +0800

    Revert "Revert "Init BE""
    This reverts commit adf79c897e9ac651a1769efb1a7d37769a4e8e58.

---
+++ springboot-auth-be/src/main/java/com/csy/springbootauthbe/SpringbootAuthBeApplication.java
@@ -0,0 +1,13 @@
+package com.csy.springbootauthbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuthBeApplication.class, args);
+	}
+
+}
---
+++ springboot-auth-be/src/test/java/com/csy/springbootauthbe/SpringbootAuthBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csy.springbootauthbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
